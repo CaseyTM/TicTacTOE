d0ecfa5626df4838055b2d49a09b6f767467095a
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2016-12-08 10:54:17 -0500

    working on structure

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// User should be able to click box, and have a mark show up.
+// put an onclick in the first square
+// when user clicks, call function that places x in the box
+// MILESTONE
+
+function markSquare(square){
+	console.log(square.id);
+}
